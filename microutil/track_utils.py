961329d58005f8219ed1b95d85dfdbd0c23ca77e
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-02-09 10:33:17 -0500

    add utility functions for checking cell labels

--- microutil/track_utils.py
+++ microutil/track_utils.py
@@ -1,8 +1,12 @@
 __all__ = [
     "reindex_labels",
     "reindex_labels_return",
+    "check_cell_numbers",
+    "find_duplicate_labels",
 ]
 import numpy as np
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
 
 
 def _reindex_labels(ids, areas, frame, min_area):
@@ -66,3 +70,97 @@ def reindex_labels(frame, min_area=None):
         The frame with small patches removed and the labels do not skip any integers.
     """
     return reindex_labels_return(frame, min_area)[0]
+
+
+def check_cell_numbers(BF, mask, check_after=True, correct=True, bad_frames=None, min_area=25):
+    """
+    check if the number of cells ever decreases from one frame to the next. If there is a decrease
+    and *correct* is True then open napari to allow for manual correction.
+    WARNING: currently only allows for a time dimesion. Will need updating for more complex indexing
+
+    Parameters
+    ----------
+    BF : (..., X, Y) array
+        The BF images to use as background
+    mask : (..., X, Y) array
+    check_after : bool, default: True
+        Whether to run the checking code after the manual updates.
+    correct : bool, default: True
+        Whether to open Napari to correct the bad frames.
+    bad_frames : list, optional
+        If not None then this will be used instead of the initial check.
+        This is to enable calling this function in a loop and not doubling up on checks.
+    min_area : number or None, default: 25
+
+    Returns
+    -------
+    bad_frames : list, or None
+        If *check_after* is True then this will be frames after
+
+    Examples
+    --------
+    >>> bad_frames = check_cell_numbers(BF, tracked)
+    >>> while bad_frames is not None:
+        print("========")
+        print(bad_frames)
+        bad_frames = check_cell_numbers(BF, tracked)
+
+    """
+    if mask.ndim != 3:
+        raise ValueError(
+            "Not yet supported - If you run into this error"
+            "then it is time to start supporting this"
+            "tell Ian to get it together and make it happen..."
+        )
+
+    def _check(arr):
+        for i in range(len(arr)):
+            mask[i] = mu.track_utils.reindex_labels(arr[i], min_area=min_area)
+        bad_frames = []
+        for i in range(1, len(mask)):
+            prev_N = len(np.unique(mask[i - 1]))
+            curr_N = len(np.unique(mask[i]))
+            if prev_N > curr_N:
+                print(i, prev_N, curr_N)
+                bad_frames.append(i)
+        return bad_frames
+
+    def _empty_check(l):
+        if len(l) == 0:
+            return None
+
+    if bad_frames is None:
+        bad_frames = _check(mask)
+
+    if correct:
+        for i in bad_frames:
+            fixed = mu.manual_segmentation(BF[i - 1 : i + 1], mask[i - 1 : i + 1])
+            mask[i - 1 : i + 1] = fixed
+    else:
+        return _empty_check(bad_frames)
+
+    if check_after:
+        return _empty_check(_check(mask))
+    return None
+
+
+def find_duplicate_labels(frame):
+    """
+    Open a plot with a slider to tanually search a frame
+    in order to check if any two cells share a label.
+
+    Parameters
+    ----------
+    frame : (X, Y) array-like of int
+        The labels of the frame.
+    """
+    slider = widgets.IntSlider(min=0, max=np.max(np.unique(frame)))
+    fig = plt.figure()
+    im = plt.imshow(frame == 0)
+
+    def update(change):
+        im.set_data(frame == change['new'])
+        fig.canvas.draw()
+
+    slider.observe(update, names='value')
+    display(slider)
