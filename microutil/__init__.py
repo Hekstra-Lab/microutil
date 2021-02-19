9b3c3323ecb8fdfdb87a4d26cddfe1686f5c19be
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-02-19 15:25:59 -0500

    modernize packaging

--- microutil/__init__.py
+++ microutil/__init__.py
@@ -1,4 +1,6 @@
-from ._version import __version__, version_info
+# Must import __version__ first to avoid errors importing this file during the build process.
+# See https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
+from ._version import __version__
 from .loading import *
 from .preprocess import *
 from .masks import *
