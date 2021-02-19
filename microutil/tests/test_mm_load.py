7861c14ef8820bf3b31a776979eccaf06717d288
John Russell <johncrussell25@gmail.com>
2021-02-19 16:38:23 -0500

    comment about what is going on for my future self

--- microutil/tests/test_mm_load.py
+++ microutil/tests/test_mm_load.py
@@ -7,6 +7,12 @@ import os
 dir_ = Path(os.path.abspath(__file__)).parent
 
 def test_load_mm_frames():
+    """
+    For future reference the elements of the images were overwritten to be small 2x2 arrays.
+    The elements of each array are:
+        [[channel, position],
+         [time   ,     z   ]]
+    """
     loaded = load_mm_frames(str(dir_.joinpath('test-data/load_mm_frames/input')))
     ref = da.from_zarr(str(dir_.joinpath('test-data/load_mm_frames/reference_file')))
     assert(da.all(loaded.data==ref)).compute()
