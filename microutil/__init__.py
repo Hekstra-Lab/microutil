3afca564582e0bb22ff36d256237aad65b7eda6e
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-08-26 16:22:26 -0400

    fix import order in init

--- microutil/__init__.py
+++ microutil/__init__.py
@@ -1,7 +1,7 @@
 # Must import __version__ first to avoid errors importing this file during the build process.
 # See https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
-from . import leica, single_cell
 from ._version import __version__
+from . import leica, single_cell
 from .loading import *
 from .masks import *
 from .napari_wrappers import *
