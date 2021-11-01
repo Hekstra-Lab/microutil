608e8dbbf06fff6add5bafd34871253dc8af67f6
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-11-01 17:17:28 -0400

    Require fast-overlap
    Should be easy to install everywhere

--- microutil/segmentation.py
+++ microutil/segmentation.py
@@ -8,7 +8,6 @@ __all__ = [
 ]
 
 
-import warnings
 
 import numpy as np
 import scipy.ndimage as ndi
@@ -18,13 +17,7 @@ from skimage.feature import peak_local_max
 from skimage.filters import threshold_isodata, threshold_otsu
 from skimage.morphology import label
 from skimage.segmentation import watershed
-
-try:
-    from fast_histogram import histogram1d
-except ImportError:
-    warnings.warn(
-        "Could not import fast_histogram. The function fast_otsu will fail if you call it."
-    )
+from fast_histogram import histogram1d
 
 from .track_utils import _reindex_labels
 
